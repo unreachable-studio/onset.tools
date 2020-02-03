0ce5afc8ce9f647fcf049d9fefd629bd2ccc50d9
alexandregv <contact@alexandregv.fr>
2020-02-03 15:59:07 +0100

    Resolve merge/stash conflict

---
+++ front/src/components/Packages/PackageViewAuthors.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PackageViewAuthors = ({ package: { id, name, date, author: { id, name, postCount }, views, downloads} }) => { 
+  const classes = useStyles();
+
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default PackageViewAuthors;
--- front/src/routes/Routes.js
+++ front/src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
 import cookie from 'react-cookies';
 
+import cookie from 'react-cookies';
 import Packages from '../components/Packages/Packages';
 
 const Router = () => (
