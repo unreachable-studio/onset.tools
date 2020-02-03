6a0e7f0db8436b4efbf51655d608ed2f18d7b445
alexandregv <contact@alexandregv.fr>
2020-02-03 13:37:39 +0100

    Unstash 0

---
+++ front/src/components/Packages/PackageView.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import {   
+  Container, 
+  Grid, 
+  Typography } from "@material-ui/core";
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    marginTop: '60px',
+  },
+  packageName: {
+    marginLeft: '10px'
+  },
+}));
+
+const PackageViewItem = ({ package: { id, name, date, author, views, downloads, description } }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+const PackageView = (props) => {
+  const { match } = props;
+  let {id} = match.params;
+
+  const classes = useStyles();
+  const packageContent = [{ id: 1, name: "First package",  date: "01/02/2003", author: "Thalos", views: 1337, downloads: 256, short_description: "The veefhfffewnfefewf ef eufew fuew feuw feu. fewh fry first package.", long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis vel nisl in feugiat. Integer hendrerit felis sollicitudin eros feugiat, ac efficitur dui luctus. Morbi pellentesque risus eu hendrerit tincidunt. Sed tempus dolor at finibus commodo. Integer mi est, gravida quis enim ac, vehicula dignissim urna. Duis aliquet, neque eu rhoncus egestas, mi lectus varius eros, eget luctus libero velit imperdiet sem. Praesent fermentum, metus vel iaculis eleifend, lorem turpis auctor metus, rutrum porttitor mi purus vel massa. Nulla facilisi. Duis euismod massa arcu, vel fermentum sem vulputate ac. Maecenas imperdiet tincidunt congue. Pellentesque sit amet congue nulla. Vestibulum ut dignissim lectus. Duis id mollis leo." }]
+  return (
+    <>
+      <Container className={classes.root}>
+        <Grid container grid={1}>
+          <Grid item xs={12}>
+          { packageContent.map((p, index) => <PackageViewItem key={index} package={p} />) }
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+export default PackageView;
--- front/src/routes/Routes.js
+++ front/src/routes/Routes.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
 
 import Packages from '../components/Packages/Packages';
+import PackageView from "../components/Packages/PackageView";
 
 const Router = () => (
     <Switch>
 		<Route exact path="/packages" component={Packages} />
+        <Route exact path="/package/:id" component={PackageView} />
+		<Route exact path="/logout"  component={() => { cookie.remove('token'); return <Redirect to="/" />} }/>
         <Redirect to="/packages" />
     </Switch>
 )
