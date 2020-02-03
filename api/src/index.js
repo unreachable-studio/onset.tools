a7c6e792c9aca172bfe4b384d2e85c4833cec00d
alexandregv <contact@alexandregv.fr>
2020-02-03 12:11:16 +0100

    Add Steam OpenID (back-end)

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,63 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const passport = require('passport');
+const SteamStrategy = require('passport-steam');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+//app.use(session({
+//    secret: 'onset',
+//    name: 'namesessid',
+//    resave: true,
+//    saveUninitialized: true
+//}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+passport.use(new SteamStrategy({
+  returnURL: 'http://unreachable.fr:4001/auth/steam/return',
+  realm: 'http://unreachable.fr:4001/',
+  apiKey: '5461922463F2C0C67244481E77B2C103'
+},
+  function(identifier, profile, done) {
+	//User.findByOpenID({ openId: identifier }, function (err, user) {
+	//  return done(err, user);
+	//});
+	return done(null, profile);
+  }
+));
+
+app.get('/auth/steam',
+  passport.authenticate('steam'),
+  function(req, res) {
+	// The request will be redirected to Steam for authentication, so
+	// this function will not be called.
+  });
+
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/packages' }), //TODO: change failure url
+  function(req, res) {
+	const token = jwt.sign({ user: req.user }, /*process.env.SECRET_KEY*/"onset", { expiresIn: '2h' });
+	  res.cookie('token', token);
+	  res.redirect('http://unreachable.fr:3001/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!')
--- front/src/components/NavBar/NavBar.js
+++ front/src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -21,40 +21,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const useStyles = makeStyles({
   list: {
-    width: 270,
+	width: 270,
   },
   loginText: {
-    marginLeft: '5px'
+	marginLeft: '5px'
   },
   link: {
-	  textDecoration: 'none',
-	  color: '#202020',
+	textDecoration: 'none',
+	color: '#202020',
   },
   footer: {
-    position: 'absolute',
-    display: 'block',
-    width: '100%',
-    bottom: '10px',
+	position: 'absolute',
+	display: 'block',
+	width: '100%',
+	bottom: '10px',
   },
   externalLinks: {
-    display: 'block',
-    marginLeft: 'auto',
-    textAlign: 'center',
-    paddingTop: '20px'
+	display: 'block',
+	marginLeft: 'auto',
+	textAlign: 'center',
+	paddingTop: '20px'
   },
   externalLinksBtn: {
-    textDecoration: 'none',
-    color: '#202020',
-    marginLeft: '10px',
-    marginRight: '10px'
+	textDecoration: 'none',
+	color: '#202020',
+	marginLeft: '10px',
+	marginRight: '10px'
   },
   unreachable: {
-    textAlign: 'center',
-    marginTop: '5px'
+	textAlign: 'center',
+	marginTop: '5px'
   },
   unreachableBtn: {
-    textDecoration: 'none',
-    color: '#202020',
+	textDecoration: 'none',
+	color: '#202020',
   },
 });
 
@@ -62,54 +62,71 @@ const NavBar = () => {
   const classes = useStyles();
   const [state, setState] = useState(false);
 
+  useEffect(() => {
+	window.addEventListener('message', event => {
+	  if (event.origin !== process.env.REACT_APP_API_URL) return;
+
+	  const { token, ok } = event.data;
+
+	  if (ok) {
+		localStorage.setItem('authToken', token);
+		console.log(token);
+	  }
+	});
+  }, [])
+
   const toggleDrawer = open => event => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift'))
-      return;
-    setState(open);
+	if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift'))
+	  return;
+	setState(open);
   };
 
   const Menu = () => (
-    <div className={classes.list} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
-      <List>
-		  <NavLink to="/packages" className={classes.link} activeClassName="activeLink">
-            <ListItem button key={0}>
-              <ListItemIcon className="iconNavbar"><FontAwesomeIcon icon="cube" size="1x"/></ListItemIcon>
-              <ListItemText primary="Packages" />
-            </ListItem>
-        </NavLink>
+	<div className={classes.list} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+	  <List>
+		<NavLink to="/packages" className={classes.link} activeClassName="activeLink">
+		  <ListItem button key={0}>
+			<ListItemIcon className="iconNavbar"><FontAwesomeIcon icon="cube" size="1x"/></ListItemIcon>
+			<ListItemText primary="Packages" />
+		  </ListItem>
+		</NavLink>
 
-        <NavLink to="/assets" className={classes.link} activeClassName="activeLink">
-          <ListItem button key={1}>
-            <ListItemIcon className="iconNavbar"><FontAwesomeIcon icon="image" size="1x"/></ListItemIcon>
-            <ListItemText primary="Assets" />
-          </ListItem>
-        </NavLink>
-      </List>
+		<NavLink to="/assets" className={classes.link} activeClassName="activeLink">
+		  <ListItem button key={1}>
+			<ListItemIcon className="iconNavbar"><FontAwesomeIcon icon="image" size="1x"/></ListItemIcon>
+			<ListItemText primary="Assets" />
+		  </ListItem>
+		</NavLink>
+	  </List>
 
 
-      <div className={classes.footer}>
-        <Divider />
-        <div className={classes.externalLinks}>
-          <a href="https://discordapp.com/invite/C8qSa2K" className={classes.externalLinksBtn}><FontAwesomeIcon icon={['fab', 'discord']} size="2x"/></a>
-          <a href="https://github.com/unreachable-studio/onset.tools" className={classes.externalLinksBtn}><FontAwesomeIcon icon={['fab', 'github']} size="2x"/></a>
-        </div>
-        <div className={classes.unreachable}><a href="https://unreachable.studio/" className={classes.unreachableBtn}>Unreachable Studio</a></div>
-      </div>
-    </div>
+	  <div className={classes.footer}>
+		<Divider />
+		<div className={classes.externalLinks}>
+		  <a href="https://discordapp.com/invite/C8qSa2K" className={classes.externalLinksBtn}><FontAwesomeIcon icon={['fab', 'discord']} size="2x"/></a>
+		  <a href="https://github.com/unreachable-studio/onset.tools" className={classes.externalLinksBtn}><FontAwesomeIcon icon={['fab', 'github']} size="2x"/></a>
+		</div>
+		<div className={classes.unreachable}><a href="https://unreachable.studio/" className={classes.unreachableBtn}>Unreachable Studio</a></div>
+	  </div>
+	</div>
   );
 
-  return <>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton onClick={toggleDrawer(true)} edge="start" color="inherit" aria-label="menu"><MenuIcon /></IconButton>
-        <Typography variant="h6">Onset.tools</Typography>
-        <Button style={{marginLeft: 'auto'}} color="inherit" aria-label="login"><FontAwesomeIcon icon={['fab', 'steam']} /> <span className={classes.loginText}>Login</span></Button>
-      </Toolbar>
-    </AppBar>
+  const handleLogin = () => {
+	window.location.href = process.env.REACT_APP_API_URL + '/auth/steam';
+  }
 
-    <Drawer open={state} onClose={toggleDrawer(false)}>
-      <Menu /> 
-    </Drawer>
+  return <>
+	<AppBar position="static">
+	  <Toolbar>
+		<IconButton onClick={toggleDrawer(true)} edge="start" color="inherit" aria-label="menu"><MenuIcon /></IconButton>
+		<Typography variant="h6">Onset.tools</Typography>
+		<Button onClick={handleLogin} style={{marginLeft: 'auto'}} color="inherit" aria-label="login"><FontAwesomeIcon icon={['fab', 'steam']} /> <span className={classes.loginText}>Login</span></Button>
+	  </Toolbar>
+	</AppBar>
+
+	<Drawer open={state} onClose={toggleDrawer(false)}>
+	  <Menu /> 
+	</Drawer>
   </>
 }
 
