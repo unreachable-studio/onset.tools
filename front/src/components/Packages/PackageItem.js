a541cdfe6c0261971e4fd4a352ede23fb330a76c
baptistemrt <bapmarty@gmail.com>
2020-03-29 19:02:01 +0200

    Change package page front

--- front/src/components/Packages/PackageItem.js
+++ front/src/components/Packages/PackageItem.js
@@ -1,81 +1,81 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Paper from '@material-ui/core/Paper';
 
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
   Button,
-  IconButton,
   Typography,
   Grid,
 } from '@material-ui/core/';
 
-//import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-	maxWidth: 445,
-  },
   view: {
-	marginLeft: 'auto',
-	textDecoration: 'none',
+		marginLeft: 'auto',
+		textDecoration: 'none',
   },
   eye: {
-	display: 'inline-block',
-	marginLeft: '5px',
-	marginRight: '10px',
+		display: 'inline-block',
+		marginLeft: '5px',
+		marginRight: '10px',
   },
   download: {
-	display: 'inline-block',
-  }
+		display: 'inline-block',
+	},
+	paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.primary,
+  },
 }));
 
 const PackageItem = ({ package: { id, name, date, author, views, downloads, short_description }, ...props }) => {
   const classes = useStyles();
-
-  const theme = useTheme();
-  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
-
   return (
-	  <Grid item style={mobile ? {width: '100%'} : null}>
-	  <Card className={classes.root}>
-
-		<CardHeader
-		  avatar={<FontAwesomeIcon icon="cube" size="2x" />}
-		  action={
-			<IconButton aria-label="settings">
-			  <FavoriteBorderIcon style={{fontSize: '90%'}}/>
-			</IconButton>
-		  }
-		  title={name}
-		  subheader={date + ' • ' + author}
-		/>
-
-		<CardContent>
-		  <Typography variant="body2" color="textSecondary" component="p">{short_description}</Typography>
-		</CardContent>
-
-		<CardActions disableSpacing>
-		  <div className={classes.eye}><FontAwesomeIcon icon="eye" /> {views}</div>
-		  <div className={classes.download}><FontAwesomeIcon icon="download" /> {downloads}</div>
-
-		  <Link to={"/package/" + id} className={classes.view}>
-			<Button aria-label="view">View</Button>
-		  </Link>
-		</CardActions>
-
-	  </Card>
-	</Grid>
-  );
+		<>    
+    <Grid item xs={12}>
+			<Paper className={classes.paper} elevation={3}>
+			<Grid container spacing={2}>
+          <Grid item>
+						<FontAwesomeIcon icon="cube" size="2x" />
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+									{name}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+									{short_description}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+									{date + ' • ' + author}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item direction="column">
+							<Grid item>
+								<Typography variant="subtitle1">
+									<div className={classes.eye}><FontAwesomeIcon icon="eye" /> {views}</div>
+									<div className={classes.download}><FontAwesomeIcon icon="download" /> {downloads}</div>
+								</Typography>
+							</Grid>
+							<Grid item container justify="flex-end">
+								<Grid item>
+									<Link to={"/package/" + id} className={classes.view}>
+										<Button aria-label="view">View</Button>
+									</Link>
+								</Grid>
+							</Grid>
+						</Grid>
+          </Grid>
+        </Grid>
+			</Paper>
+		</Grid>
+	</>
+	);
 }
 
 export default PackageItem;
